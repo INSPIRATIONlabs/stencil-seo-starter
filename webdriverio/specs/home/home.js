be92b24c73a3cfd4c2413e26e3ac64c1ae05d3c7
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2019-06-18 23:08:21 +0200

    test(): Simplified tests

--- webdriverio/specs/home/home.js
+++ webdriverio/specs/home/home.js
@@ -4,7 +4,7 @@ before(function () {
   chai.Should();
 });
 
-describe('home page', function () {
+describe('home pages', function () {
   it('should be able to render the page for /', function () {
     browser.url('/'); // open url
 
@@ -12,5 +12,23 @@ describe('home page', function () {
 
     expect(headline).to.be.a('string');
     expect(headline).equal('Please choose your language');
+  });
+
+  it('should be able to render the page for /en', function () {
+    browser.url('/en'); // open url
+
+    const headline = browser.getText('h1');
+
+    expect(headline).to.be.a('string');
+    expect(headline).equal('Welcome to the stencil SEO starter');
+  });
+
+  it('should be able to render the page for /de', function () {
+    browser.url('/de'); // open url
+
+    const headline = browser.getText('h1');
+
+    expect(headline).to.be.a('string');
+    expect(headline).equal('Willkommen im Stencil SEO Startpaket');
   });
 });
