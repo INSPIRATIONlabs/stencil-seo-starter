6d4a0d6d1b1b5dbb3e4667579cccbf5c863e9faf
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2019-10-16 13:20:25 +0200

    fix: fixed eslint error

--- src/shared/helperClass.tsx
+++ src/shared/helperClass.tsx
@@ -1,5 +1,4 @@
 export class Helper {
-
   public static addClassesToClassList(classes: string[] | string, classListBase: HTMLElement) {
     if (Array.isArray(classes) && classes.every(c => c && typeof c === "string")) {
       classListBase.classList.add(...classes);
